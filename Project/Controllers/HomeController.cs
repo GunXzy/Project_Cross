9e4fbd174a85bb9cc1220e649b39d8188c659622
GunXzy <tokiatpbvip1@gmail.com>
2022-01-11 11:26:49 +0700

    Add project files.

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project
+{
+    public class HomeController : Controller
+    {
+        private Entities_new db = new Entities_new();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Stores.ToList());
+        }
+
+        // GET: Home/Details/5
+        /*public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }*/
+
+        // GET: Home/Create
+        /*public ActionResult Create()
+        {
+            return View();
+        }*/
+
+        // POST: Home/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+       /* [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "product_id,url,name,time,Review")] Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Stores.Add(store);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(store);
+        }*/
+
+        // GET: Home/Edit/5
+        /*public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }*/
+
+        // POST: Home/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        /*[HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "product_id,url,name,time,Review")] Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(store).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(store);
+        }*/
+
+        // GET: Home/Delete/5
+        /*public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }*/
+
+        // POST: Home/Delete/5
+        /*[HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Store store = db.Stores.Find(id);
+            db.Stores.Remove(store);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }*/
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
